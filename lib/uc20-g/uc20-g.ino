b3ba4674770b6dc9908b26fa032b37df7b3b4452
naoki <naoki@cps.im.dendai.ac.jp>
2017-10-13 17:29:04 +0900

    Add check code

--- lib/uc20-g/uc20-g.ino
+++ lib/uc20-g/uc20-g.ino
@@ -1,9 +1,23 @@
+#include "UC20G.h"
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(5,4); // RX, TX
+UC20G myUC20G(&mySerial);
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  Serial.println("");
+  Serial.println("");
+  
+  bool sora = myUC20G.init("SORACOM.IO", "sora", "sora");
+  Serial.println(sora);
+  delay(20000);
+  sora = myUC20G.disconnect();
+  Serial.println(sora);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+  
 }
