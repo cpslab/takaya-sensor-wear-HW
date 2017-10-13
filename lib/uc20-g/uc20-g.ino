165090379757fcd4d91db05a68ece7fd2bffecab
naoki <naoki@cps.im.dendai.ac.jp>
2017-10-13 20:18:09 +0900

    Interactive test

--- lib/uc20-g/uc20-g.ino
+++ lib/uc20-g/uc20-g.ino
@@ -1,6 +1,6 @@
 #include "UC20G.h"
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(5,4); // RX, TX
+SoftwareSerial mySerial(5, 4); // RX, TX
 UC20G myUC20G(&mySerial);
 void setup() {
   // put your setup code here, to run once:
@@ -8,16 +8,29 @@ void setup() {
 
   Serial.println("");
   Serial.println("");
-  
-  bool sora = myUC20G.init("SORACOM.IO", "sora", "sora");
-  Serial.println(sora);
-  delay(20000);
-  sora = myUC20G.disconnect();
-  Serial.println(sora);
+
+//  bool sora = myUC20G.init("SORACOM.IO", "sora", "sora");
+//  Serial.println(sora);
+//  delay(20000);
+//  sora = myUC20G.disconnect();
+//  Serial.println(sora);
 }
 
+char input = ' ';
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  
+  if(Serial.available()){
+    input = Serial.read();
+  }
+  switch(input){
+    case 'a':
+      myUC20G.init("SORACOM.IO", "sora", "sora");
+      break;
+    case 'b':
+      myUC20G.disconnect();
+      break;
+    case 'c':
+      myUC20G.disable();
+      break;
+  }
 }
