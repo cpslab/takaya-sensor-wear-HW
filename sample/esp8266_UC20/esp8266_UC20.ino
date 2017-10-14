46e2e7e579fd54e6924e510501a67862d88f1a89
naoki <naoki@cps.im.dendai.ac.jp>
2017-10-14 17:55:01 +0900

    Add header

--- sample/esp8266_UC20/esp8266_UC20.ino
+++ sample/esp8266_UC20/esp8266_UC20.ino
@@ -1,3 +1,14 @@
+/**************************************************************/
+/*  esp8266_UC_20 with UC20-G 3G/LTE module                   */
+/*  For TAKAYA Work Wear Project                              */
+/*  Base sketch by Naoki                                      */
+/*                                                            */
+/*  Author:    Naoki Nonaka                                   */
+/*  Date:      2017/10/13.                                    */
+/*  MPU : ESP-8266                                            */
+/*                                                            */
+/**************************************************************/
+
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(5,4); // RX, TX
 void setup()
