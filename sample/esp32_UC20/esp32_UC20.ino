d004aacd20c1465071a2ead45302d80fc48b8357
naoki <naoki@cps.im.dendai.ac.jp>
2017-12-19 01:04:01 +0900

    modify TX RX

--- sample/esp32_UC20/esp32_UC20.ino
+++ sample/esp32_UC20/esp32_UC20.ino
@@ -1,13 +1,30 @@
 HardwareSerial Serial2(1); // RX, TX
 void setup()
 {
- Serial.begin(115200);
- Serial2.begin(115200, SERIAL_8N1, 17, 16);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, 16, 17);
 }
 void loop()
 {
- if(Serial2.available())
- Serial.write(Serial2.read());
- if (Serial.available())
- Serial2.write(Serial.read());
+  if (Serial2.available())
+    Serial.write(Serial2.read());
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == '/') {
+      Serial.println("enable");
+      enable();
+    }
+    else {
+      Serial2.write(c);
+    }
+  }
 }
+
+void enable() {
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
+}
+
